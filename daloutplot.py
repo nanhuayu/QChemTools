6502b5e3e2507ddc01c2ceaeaa108d450a124493
nanhuayu <nanhuayu@gmail.com>
2018-09-29 22:10:44 +0800

    180929

--- daloutplot.py
+++ daloutplot.py
@@ -1,10 +1,15 @@
 import os, sys
 
 import parsedalton
+import parsegaussian
 import molplot
 
 print(sys.argv)
 filename = sys.argv[1]
 
-mol = parsedalton.parseout(filename)
+if filename.lower().endswith('gjf'): mol = parsegaussian.parsegif(filename)
+elif filename.lower().endswith('log'): mol = parsegaussian.parselog(filename)
+elif filename.lower().endswith('mol'): mol = parsedalton.parsemol(filename)
+elif filename.lower().endswith('out'): mol = parsedalton.parseout(filename)
+
 molplot.plot_molecule(mol)
